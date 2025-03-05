bdc346b8e760505745186cf4658a80a818c6ce5d
Kenny Vo <kevo001@student.kristiania.no>
2025-03-05 15:09:54 +0100

    Initial Commit

---
+++ frontend/client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Hello from './components/Hello';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Art Gallery Ecommerce</h1>
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
