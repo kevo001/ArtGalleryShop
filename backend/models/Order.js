478c722d83ef1087ee63eb067aa6307831632825
Kenny Vo <kevo001@student.kristiania.no>
2025-04-13 21:03:44 +0200

    Modified Order values

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const orderSchema = new mongoose.Schema({
   orderNumber: { type: String, required: true },
   customerName: { type: String, required: true },
+  email: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  address: { type: String, required: true },
   status: { type: String, enum: ['Under behandling', 'Ferdig'], default: 'Under behandling' },
   date: { type: String, required: true },
   totalAmount: { type: Number, required: true }
