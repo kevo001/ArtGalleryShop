0c4f34f7132a38739093a19df5231ab4f291f28e
Kenny Vo <kevo001@student.kristiania.no>
2025-04-23 15:30:58 +0200

    Fixed problems

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -3,14 +3,24 @@ const mongoose = require('mongoose');
 const orderSchema = new mongoose.Schema({
   orderNumber: { type: String, required: true },
   customerName: { type: String, required: true },
-  email: { type: String, required: true },
-  phoneNumber: { type: String, required: true },
-  address: { type: String, required: true },
+  email: { type: String },
+  address: {
+    line1: String,
+    line2: String,
+    city: String,
+    postal_code: String,
+    country: String,
+  },
+  cart: [
+    {
+      title: String,
+      quantity: Number,
+      price: Number,
+    }
+  ],
   status: { type: String, enum: ['Under behandling', 'Ferdig'], default: 'Under behandling' },
   date: { type: String, required: true },
   totalAmount: { type: Number, required: true }
 });
 
-const Order = mongoose.model('Order', orderSchema);
-
-module.exports = Order;
+module.exports = mongoose.models.Order || mongoose.model('Order', orderSchema);
