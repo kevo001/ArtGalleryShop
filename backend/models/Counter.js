85f3e649f3c9192ffcd73fc2eaa543e1e0935598
Kenny Vo <kevo001@student.kristiania.no>
2025-04-23 14:47:25 +0200

    Revert "Added changes from jens-branch"
    This reverts commit 5d92b0fe5d1caa013e15297ea255282c619f9592.

--- backend/models/Counter.js
+++
@@ -1,9 +0,0 @@
-
-const mongoose = require('mongoose');
-
-const counterSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  value: { type: Number, default: 1000 },
-});
-
-module.exports = mongoose.models.Counter || mongoose.model('Counter', counterSchema);
